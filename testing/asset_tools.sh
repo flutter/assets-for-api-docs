788ec06f4c4363e5161d764f069c544b71999784
Greg Spencer <gspencer@google.com>
2018-04-23 14:49:31 -0700

    Add diagram tests to Travis

--- testing/asset_tools.sh
+++ testing/asset_tools.sh
@@ -4,7 +4,11 @@ set -e
 if [[ "$1" == "analyze" ]]; then
   echo "Analyzing Dart files."
   (cd src && flutter analyze)
+  (cd packages/diagram && flutter analyze)
+  (cd packages/diagram/example/simple && flutter analyze)
+
 elif [[ "$1" == "test" ]]; then
   echo "Running tests."
   (cd src && flutter test)
+  (cd packages/diagram && flutter test)
 fi
