09fd3ff134a9a0811181f8330593618c21ac5022
Shi Hao Hong <shihaohong94@gmail.com>
2019-04-08 14:10:47 -0700

    implement second custom item

--- packages/diagrams/lib/src/custom_list_item.dart
+++ packages/diagrams/lib/src/custom_list_item.dart
@@ -96,6 +96,130 @@ class CustomListItem extends StatelessWidget {
   }
 }
 
+class _ArticleDescription extends StatelessWidget {
+  _ArticleDescription({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '$title',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              Text(
+                '$subtitle',
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          flex: 3,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                '$author',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '$publishDate · $readDuration ★',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          flex: 2,
+        ),
+      ],
+    );
+  }
+}
+
+class CustomListItemTwo extends StatelessWidget {
+  CustomListItemTwo({
+    Key key,
+    this.thumbnail,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final Widget thumbnail;
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: thumbnail,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 class CustomListItemDiagram extends StatelessWidget implements DiagramMetadata {
   const CustomListItemDiagram(this.name);
 
@@ -104,37 +228,82 @@ class CustomListItemDiagram extends StatelessWidget implements DiagramMetadata {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      key: UniqueKey(),
-      constraints: BoxConstraints.tight(const Size(400.0, 250.0)),
-      child: Container(
-        alignment: FractionalOffset.center,
-        padding: const EdgeInsets.all(5.0),
-        color: Colors.white,
-        child: ListView(
-          padding: const EdgeInsets.all(8.0),
-          itemExtent: 106.0,
-          children: <CustomListItem>[
-            CustomListItem(
-              user: 'Flutter',
-              viewCount: 999000,
-              thumbnail: Container(
-                decoration: BoxDecoration(color: Colors.blue),
-              ),
-              title: 'The Flutter YouTube Channel',
+    switch (name) {
+      case 'custom_list_item_a':
+        return ConstrainedBox(
+          key: UniqueKey(),
+          constraints: BoxConstraints.tight(const Size(400.0, 235.0)),
+          child: Container(
+            alignment: FractionalOffset.center,
+            padding: const EdgeInsets.all(5.0),
+            color: Colors.white,
+            child: ListView(
+              padding: const EdgeInsets.all(8.0),
+              itemExtent: 106.0,
+              children: <CustomListItem>[
+                CustomListItem(
+                  user: 'Flutter',
+                  viewCount: 999000,
+                  thumbnail: Container(
+                    decoration: BoxDecoration(color: Colors.blue),
+                  ),
+                  title: 'The Flutter YouTube Channel',
+                ),
+                CustomListItem(
+                  user: 'Dash',
+                  viewCount: 884000,
+                  thumbnail: Container(
+                    decoration: BoxDecoration(color: Colors.yellow),
+                  ),
+                  title: 'Announcing Flutter 1.0',
+                ),
+              ],
             ),
-            CustomListItem(
-              user: 'Dash',
-              viewCount: 884000,
-              thumbnail: Container(
-                decoration: BoxDecoration(color: Colors.yellow),
-              ),
-              title: 'Announcing Flutter 1.0',
+          ),
+        );
+        break;
+      case 'custom_list_item_b':
+       return ConstrainedBox(
+          key: UniqueKey(),
+          constraints: BoxConstraints.tight(const Size(400.0, 265.0)),
+          child: Container(
+            alignment: FractionalOffset.center,
+            padding: const EdgeInsets.all(5.0),
+            color: Colors.white,
+            child: ListView(
+              padding: const EdgeInsets.all(10.0),
+              children: <Widget>[
+                CustomListItemTwo(
+                  thumbnail: Container(
+                    decoration: BoxDecoration(color: Colors.pink),
+                  ),
+                  title: 'Flutter 1.0 Launch',
+                  subtitle:
+                    'Flutter continues to improve and expand its horizons.'
+                    'This text should max out at two lines and clip',
+                  author: 'Dash',
+                  publishDate: 'Dec 28',
+                  readDuration: '5 mins',
+                ),
+                CustomListItemTwo(
+                  thumbnail: Container(
+                    decoration: BoxDecoration(color: Colors.blue),
+                  ),
+                  title: 'Flutter 1.2 Release - Continual updates to the framework',
+                  subtitle: 'Flutter once again improves and makes updates.',
+                  author: 'Flutter',
+                  publishDate: 'Feb 26',
+                  readDuration: '12 mins',
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+       );
+        break;
+      default:
+        return const Text('Error');
+        break;
+    }
   }
 }
 
@@ -146,7 +315,8 @@ class CustomListItemDiagramStep extends DiagramStep {
 
   @override
   Future<List<DiagramMetadata>> get diagrams async => <DiagramMetadata>[
-    const CustomListItemDiagram('custom_list_item'),
+    const CustomListItemDiagram('custom_list_item_a'),
+    const CustomListItemDiagram('custom_list_item_b'),
   ];
 
   @override
