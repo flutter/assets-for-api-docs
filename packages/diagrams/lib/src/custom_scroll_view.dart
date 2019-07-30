4a4f16c1a869ea8b2a08ec749ea4e11e971ad7d1
Brian Egan <brian@brianegan.com>
2019-07-30 19:41:07 +0200

    Create video of `CustomScrollView` functionality

--- packages/diagrams/lib/src/custom_scroll_view.dart
+++ packages/diagrams/lib/src/custom_scroll_view.dart
@@ -7,66 +7,136 @@ import 'dart:io';
 
 import 'package:diagram_capture/diagram_capture.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'diagram_step.dart';
+import 'fake_drag_scroll_activity.dart';
 
+final Duration _kTotalDuration =
+    _kScrollUpDuration
+        + _kScrollPauseDuration
+        + _kScrollDownDurationPartOne
+        + _kScrollPauseDuration
+        + _kScrollDownDurationPartTwo
+        + _kScrollPauseDuration;
+const Duration _kScrollUpDuration = Duration(seconds: 1, milliseconds: 500);
+const Duration _kScrollDownDurationPartOne = Duration(milliseconds: 800);
+const Duration _kScrollDownDurationPartTwo = Duration(seconds: 1);
+const Duration _kScrollPauseDuration = Duration(milliseconds: 900);
+
+const double _kCurveAnimationFrameRate = 60.0;
 const String _customScrollView = 'custom_scroll_view';
 
-class CustomScrollViewDiagram extends StatelessWidget implements DiagramMetadata {
+class CustomScrollViewDiagram extends StatefulWidget implements DiagramMetadata {
   const CustomScrollViewDiagram(this.name);
 
   @override
   final String name;
 
   @override
+  State<StatefulWidget> createState() => _CustomScrollViewDiagramState();
+
+}
+
+class _CustomScrollViewDiagramState extends State<CustomScrollViewDiagram> with TickerProviderStateMixin<CustomScrollViewDiagram> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.scheduleFrameCallback((Duration _) {
+      _play();
+    });
+  }
+
+  @override
+  void didUpdateWidget(CustomScrollViewDiagram oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _scrollController.jumpTo(0.0);
+    SchedulerBinding.instance.scheduleFrameCallback((Duration _) {
+      _play();
+    });
+  }
+
+  Future<void> _play() async {
+    await Future<void>.delayed(_kScrollPauseDuration);
+    await _animate(
+      to: 650.0,
+      duration: _kScrollUpDuration,
+    );
+    await Future<void>.delayed(_kScrollPauseDuration);
+    await _animate(
+      to: 500.0,
+      duration: _kScrollDownDurationPartOne,
+    );
+    await Future<void>.delayed(_kScrollPauseDuration);
+    await _animate(
+      to: 0.0,
+      duration: _kScrollDownDurationPartTwo,
+    );
+  }
+
+  Future<void> _animate({double to, Duration duration}) {
+    final ScrollPositionWithSingleContext position = _scrollController.position;
+    final FakeDragScrollActivity activity = FakeDragScrollActivity(
+      position,
+      from: _scrollController.offset,
+      to: to,
+      duration: duration,
+      curve: Curves.easeInOut,
+      vsync: this,
+    );
+    _scrollController.position.beginActivity(activity);
+    return activity.done;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      key: UniqueKey(),
-      constraints: BoxConstraints.tight(const Size(300.0, 533.33)),
-      child: Container(
-        padding: const EdgeInsets.all(5.0),
-        color: Colors.white,
-        child: CustomScrollView(
-          slivers: <Widget>[
-            const SliverAppBar(
-              pinned: true,
-              expandedHeight: 250.0,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text('Demo'),
-              ),
+    return Container(
+      color: Colors.white,
+      height: 400.0,
+      width: 376.0,
+      child: CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Demo'),
             ),
-            SliverGrid(
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0,
-                mainAxisSpacing: 10.0,
-                crossAxisSpacing: 10.0,
-                childAspectRatio: 4.0,
-              ),
-              delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    color: Colors.teal[100 * (index % 9)],
-                    child: Text('grid item $index'),
-                  );
-                },
-                childCount: 20,
-              ),
+          ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
             ),
-            SliverFixedExtentList(
-              itemExtent: 50.0,
-              delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    color: Colors.lightBlue[100 * (index % 9)],
-                    child: Text('list item $index'),
-                  );
-                },
-              ),
+            delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              },
+              childCount: 20,
             ),
-          ],
-        ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.lightBlue[100 * (index % 9)],
+                child: Text('list item $index'),
+              );
+            },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -86,6 +156,11 @@ class CustomScrollViewDiagramStep extends DiagramStep<CustomScrollViewDiagram> {
   @override
   Future<File> generateDiagram(CustomScrollViewDiagram diagram) async {
     controller.builder = (BuildContext context) => diagram;
-    return await controller.drawDiagramToFile(new File('${diagram.name}.png'));
+    return await controller.drawAnimatedDiagramToFiles(
+      end: _kTotalDuration,
+      frameRate: _kCurveAnimationFrameRate,
+      name: diagram.name,
+      category: category,
+    );
   }
 }
--- packages/diagrams/lib/src/sliver_app_bars.dart
+++ packages/diagrams/lib/src/sliver_app_bars.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 import 'diagram_step.dart';
+import 'fake_drag_scroll_activity.dart';
 
 final Duration _kTotalDuration =
       _kScrollUpDuration
@@ -25,84 +26,6 @@ const Duration _kScrollDownDurationPartTwo = Duration(seconds: 1);
 const Duration _kScrollPauseDuration = Duration(milliseconds: 900);
 
 const double _kCurveAnimationFrameRate = 60.0;
-
-
-/// A [ScrollActivity] that simulates a user dragging their finger over the
-/// screen to scroll.
-///
-/// We cannot use [ScrollController.animateTo] for this animation because it
-/// sets [SliverConstraints.userScrollDirection] to [ScrollDirection.idle] to
-/// indicate that there is no real user scrolling. However, [SliverAppBar]s use
-/// this property to show or hide the app bar when the user changes scrolling
-/// direction and we want to show that in the animations.
-///
-/// This implementation is inspired by [DrivenScrollActivity], but instead
-/// of using [ScrollActivityDelegate.setPixels] it uses
-/// [ScrollActivityDelegate.applyUserOffset] to simulate real scrolling and
-/// to trigger all scroll-related effects in [SliverAppBar]s.
-class FakeDragScrollActivity extends ScrollActivity {
-  FakeDragScrollActivity(
-      ScrollActivityDelegate delegate, {
-        @required double from,
-        @required double to,
-        @required Duration duration,
-        @required Curve curve,
-        @required TickerProvider vsync,
-      }) : assert(from != null),
-        assert(to != null),
-        assert(duration != null),
-        assert(duration > Duration.zero),
-        assert(curve != null),
-        super(delegate) {
-    _lastValue = from;
-    _completer = Completer<void>();
-    _controller = AnimationController.unbounded(
-      value: from,
-      debugLabel: '$runtimeType',
-      vsync: vsync,
-    )
-      ..addListener(_tick)
-      ..animateTo(to, duration: duration, curve: curve)
-          .whenComplete(_end);
-  }
-
-  Completer<void> _completer;
-  AnimationController _controller;
-
-  Future<void> get done => _completer.future;
-
-  @override
-  double get velocity => _controller.velocity;
-
-  double _lastValue;
-
-  void _tick() {
-    delegate.applyUserOffset(_lastValue - _controller.value);
-    _lastValue = _controller.value;
-  }
-
-  void _end() {
-    delegate?.goBallistic(velocity);
-  }
-
-  @override
-  void dispatchOverscrollNotification(ScrollMetrics metrics, BuildContext context, double overscroll) {
-    OverscrollNotification(metrics: metrics, context: context, overscroll: overscroll, velocity: velocity).dispatch(context);
-  }
-
-  @override
-  bool get shouldIgnorePointer => true;
-
-  @override
-  bool get isScrolling => true;
-
-  @override
-  void dispose() {
-    _completer.complete();
-    _controller.dispose();
-    super.dispose();
-  }
-}
 
 class SliverAppBarDiagram extends StatefulWidget implements DiagramMetadata {
 
