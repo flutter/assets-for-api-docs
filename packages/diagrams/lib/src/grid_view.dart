029431b9e659a0a724e7f32a04fd6a4259f0bc2e
Brian Egan <brian@brianegan.com>
2019-08-05 17:27:50 +0200

    Update packages/diagrams/lib/src/grid_view.dart
    Co-Authored-By: Kate Lovett <katelovett@google.com>

--- packages/diagrams/lib/src/grid_view.dart
+++ packages/diagrams/lib/src/grid_view.dart
@@ -31,7 +31,7 @@ class GridViewDiagram extends StatelessWidget implements DiagramMetadata {
           crossAxisSpacing: 10.0,
           crossAxisCount: 2,
           children: <Widget>[
-            Container(child: const Center(child: Text('He\'d have you all unravel at the')), color: Colors.teal[100]),
+            Container(child: const Padding(padding: EdgeInsets.all(8.0), child: Text('He\'d have you all unravel at the')), color: Colors.teal[100]),
             Container(child: const Center(child: Text('Heed not the rabble')), color: Colors.teal[200]),
             Container(child: const Center(child: Text('Sound of screams but the')), color: Colors.teal[300]),
             Container(child: const Center(child: Text('Who scream')), color: Colors.teal[400]),
