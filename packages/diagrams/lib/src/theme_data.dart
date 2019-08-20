d9b055f152f01924c011ab992544842b33056529
Brian Egan <brian@brianegan.com>
2019-08-20 11:10:02 +0200

    Update `ThemeData` example

--- packages/diagrams/lib/src/theme_data.dart
+++ packages/diagrams/lib/src/theme_data.dart
@@ -9,9 +9,15 @@ import 'package:diagram_capture/diagram_capture.dart';
 import 'package:flutter/material.dart';
 
 import 'diagram_step.dart';
+import 'utils.dart';
 
 const String _themeData = 'theme_data';
 const String _materialAppThemeData = 'material_app_theme_data';
+final GlobalKey _heroKey = GlobalKey();
+final GlobalKey _canvasKey = GlobalKey();
+final GlobalKey _bodyKey = GlobalKey();
+final GlobalKey _appBarKey = GlobalKey();
+final GlobalKey _fabKey = GlobalKey();
 
 class ThemeDataDiagram extends StatelessWidget implements DiagramMetadata {
   const ThemeDataDiagram(this.name);
@@ -25,49 +31,99 @@ class ThemeDataDiagram extends StatelessWidget implements DiagramMetadata {
 
     switch (name) {
       case _themeData:
-        returnWidget = Theme(
-          data: ThemeData(primaryColor: Colors.amber),
-          child: Builder(
-            builder: (BuildContext context) {
-              return Container(
-                width: 100,
-                height: 100,
-                color: Theme.of(context).primaryColor,
-              );
-            },
+        returnWidget = ConstrainedBox(
+          key: UniqueKey(),
+          constraints: BoxConstraints.tight(const Size(150.0, 150.0)),
+          child: Container(
+            padding: const EdgeInsets.all(5.0),
+            color: Colors.white,
+            child: Center(
+              child: Theme(
+                data: ThemeData(primaryColor: Colors.blue),
+                child: Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: 100,
+                      height: 100,
+                      color: Theme.of(context).primaryColor,
+                    );
+                  },
+                ),
+              ),
+            ),
           ),
         );
         break;
       case _materialAppThemeData:
-        returnWidget = MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(primaryColor: Colors.blue),
-          home: Builder(
-            builder: (BuildContext context) {
-              return Center(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Theme.of(context).primaryColor,
+        returnWidget = ConstrainedBox(
+          key: UniqueKey(),
+          constraints: BoxConstraints.tight(const Size(420, 533)),
+          child: Container(
+            padding: const EdgeInsets.only(right: 120),
+            color: Colors.white,
+            child: Center(
+              child: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  primaryColor: Colors.blue,
+                  accentColor: Colors.green,
+                  textTheme: TextTheme(body1: TextStyle(color: Colors.purple)),
                 ),
-              );
-            },
+                home: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Scaffold(
+                        key: _heroKey,
+                        appBar: AppBar(
+                          key: _appBarKey,
+                          title: const Text('ThemeData Demo'),
+                        ),
+                        floatingActionButton: FloatingActionButton(
+                          child: Icon(Icons.add),
+                          onPressed: () {},
+                          key: _fabKey,
+                        ),
+                        body: Center(
+                          child: Text(
+                            'Button pressed 0 times',
+                            key: _bodyKey,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: LabelPainterWidget(
+                        key: _canvasKey,
+                        heroKey: _heroKey,
+                        labels: <Label>[
+                          Label(
+                            _bodyKey,
+                            ' body1',
+                            const FractionalOffset(1.1, 0.5),
+                          ),
+                          Label(
+                            _appBarKey,
+                            ' primaryColor',
+                            const FractionalOffset(0.9, 0.5),
+                          ),
+                          Label(
+                            _fabKey,
+                            ' accentColor',
+                            const FractionalOffset(0.8, 0.5),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         );
         break;
     }
 
-    return ConstrainedBox(
-      key: UniqueKey(),
-      constraints: BoxConstraints.tight(const Size(150.0, 150.0)),
-      child: Container(
-        padding: const EdgeInsets.all(5.0),
-        color: Colors.white,
-        child: Center(
-          child: returnWidget,
-        ),
-      ),
-    );
+    return returnWidget;
   }
 }
 
@@ -86,6 +142,6 @@ class ThemeDataDiagramStep extends DiagramStep<ThemeDataDiagram> {
   @override
   Future<File> generateDiagram(ThemeDataDiagram diagram) async {
     controller.builder = (BuildContext context) => diagram;
-    return await controller.drawDiagramToFile(new File('${diagram.name}.png'));
+    return await controller.drawDiagramToFile(File('${diagram.name}.png'));
   }
 }
