bf6dd8351aadd0b02b393c5f208c25b7e6f242dc
Greg Spencer <gspencer@google.com>
2020-11-13 11:40:36 -0800

    Fix some analyzer issues

--- packages/diagrams/lib/src/bottom_navigation_bar.dart
+++ packages/diagrams/lib/src/bottom_navigation_bar.dart
@@ -37,15 +37,15 @@ class BottomNavigationBarDiagram extends StatelessWidget implements DiagramMetad
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 icon: Icon(Icons.home),
-                title: Text('Home'),
+                label: 'Home',
               ),
               BottomNavigationBarItem(
                 icon: Icon(Icons.business),
-                title: Text('Business'),
+                label: 'Business',
               ),
               BottomNavigationBarItem(
                 icon: Icon(Icons.school),
-                title: Text('School'),
+                label: 'School',
               ),
             ],
             currentIndex: 0,
