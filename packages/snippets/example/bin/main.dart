55da257923ba15c89ea4414cd5eb5bcafe3e14d3
Greg Spencer <gspencer@google.com>
2021-04-09 11:18:56 -0700

    Add the snippets package to the repo.

---
+++ packages/snippets/example/bin/main.dart
@@ -0,0 +1,44 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io' show exit;
+import 'package:file/file.dart';
+import 'package:file/local.dart';
+import 'package:args/args.dart';
+import 'package:snippets/snippets.dart';
+
+const LocalFileSystem filesystem = LocalFileSystem();
+
+void main(List<String> argList) {
+  final ArgParser parser = ArgParser();
+  parser.addOption('templates', help: 'Where to find the templates');
+  parser.addOption(
+    'file',
+    help: 'Which source file to edit samples in',
+  );
+  final ArgResults args = parser.parse(argList);
+  if (!args.wasParsed('file')) {
+    print('File containing samples to edit must be specified with the --file option.');
+    print(parser.usage);
+    exit(-1);
+  }
+
+  final SnippetDartdocParser snippetParser = SnippetDartdocParser();
+
+  final File file = filesystem.file(args['file']! as String);
+  final List<SourceElement> elements = getFileElements(file).toList();
+  snippetParser.parseFromComments(elements);
+  snippetParser.parseAndAddAssumptions(elements, file);
+  final List<CodeSample> samples =
+      elements.expand<CodeSample>((SourceElement element) => element.samples).toList();
+
+  final SnippetGenerator generator = SnippetGenerator();
+  for (final CodeSample sample in samples) {
+    print('${sample.element}.${sample.index}: $sample');
+    print('Generated:\n${generator.generateCode(sample)}');
+  }
+
+  final Iterable<SourceLine> consolidated = generator.consolidateSnippets(samples);
+  print('Consolidated:\n${consolidated.map<String>((SourceLine line) => line.text).join('\n')}');
+}
---
+++ packages/snippets/lib/snippets.dart
@@ -0,0 +1,13 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library snippets;
+
+export 'analysis.dart';
+export 'configuration.dart';
+export 'data_types.dart';
+export 'flutter_sample_editor.dart';
+export 'snippet_generator.dart';
+export 'snippet_parser.dart';
+export 'util.dart';
