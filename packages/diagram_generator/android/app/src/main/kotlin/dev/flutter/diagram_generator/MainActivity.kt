40d090126ecf80bfc55af1e32ead0e4f3f1d56aa
Andre <andre.lipke@gmail.com>
2022-08-22 18:13:05 -0400

    Rename android package (#195)

--- packages/diagram_generator/android/app/src/main/kotlin/dev/flutter/diagram_generator/MainActivity.kt
+++ packages/diagram_generator/android/app/src/main/kotlin/dev/flutter/diagram_generator/MainActivity.kt
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.example.diagram_generator
+package dev.flutter.diagram_generator
 
 import io.flutter.embedding.android.FlutterActivity
 
