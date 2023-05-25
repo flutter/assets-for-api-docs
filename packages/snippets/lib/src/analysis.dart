3d537cd78359fcd485814b84891db335119b06e3
John Ryan <ryjohn@google.com>
2023-05-25 10:04:32 -0700

    run dart format

--- packages/snippets/lib/src/analysis.dart
+++ packages/snippets/lib/src/analysis.dart
@@ -159,8 +159,8 @@ class _SourceVisitor<T> extends RecursiveAstVisitor<T> {
       List<SourceLine> comment = <SourceLine>[];
       if (node.documentationComment != null &&
           node.documentationComment!.tokens.isNotEmpty) {
-        comment =
-            _processComment(declaration.name.lexeme, node.documentationComment!);
+        comment = _processComment(
+            declaration.name.lexeme, node.documentationComment!);
       }
       elements.add(
         SourceElement(
@@ -260,7 +260,8 @@ class _SourceVisitor<T> extends RecursiveAstVisitor<T> {
       if (!isInsideMethod(node)) {
         if (node.documentationComment != null &&
             node.documentationComment!.tokens.isNotEmpty) {
-          comment = _processComment(node.name.lexeme, node.documentationComment!);
+          comment =
+              _processComment(node.name.lexeme, node.documentationComment!);
         }
         elements.add(
           SourceElement(
