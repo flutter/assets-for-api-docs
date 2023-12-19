fa0a98e7677a99f329ec2b87c1457dd39f420eac
Chun-Heng Tai <chtai@google.com>
2023-12-19 10:21:20 -0800

    Adds go router push animations

--- packages/diagrams/lib/diagrams.dart
+++ packages/diagrams/lib/diagrams.dart
@@ -42,6 +42,7 @@ export 'src/floating_action_button_location.dart';
 export 'src/font_feature.dart';
 export 'src/form.dart';
 export 'src/gesture_detector.dart';
+export 'src/go_router_push_animation.dart';
 export 'src/grid_view.dart';
 export 'src/heroes.dart';
 export 'src/icon.dart';
--- packages/diagrams/lib/src/font_feature.dart
+++ packages/diagrams/lib/src/font_feature.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:ui' show FontFeature;
 
 import 'package:flutter/material.dart';
 
